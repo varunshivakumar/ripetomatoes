69389a5c7e4e121be74941d07c533ae58e915e16
Varun Shivakumar <varunshivakumar@outlook.com>
2017-08-24 20:30:47 -0400

    Load more movies implemented

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,12 +2,20 @@ class VisitorsController < ApplicationController
 
 
   def index
-    api_key = "5c6aa64f2306d670d1918784955b7812"
-    request = HTTP.get("https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=en-US")
+
+    @page = params[:page].nil? ? 1 : params[:page]
+
+    @api_key = "5c6aa64f2306d670d1918784955b7812"
+    request = HTTP.get("https://api.themoviedb.org/3/movie/popular?page=#{@page}&api_key=#{@api_key}&language=en-US")
     @data = JSON.parse(request)
+
     @movies = @data['results']
-    @page = @data['page']
     @total_pages = @data['total_pages']
     @total_results = @data['total_results']
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
