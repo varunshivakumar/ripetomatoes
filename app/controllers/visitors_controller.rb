ef952fe6ff7564c142194bb3220854f5d5714dce
Varun Shivakumar <varunshivakumar@outlook.com>
2017-08-25 02:04:20 -0400

    Implemented basic sorting using api:

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,9 +4,10 @@ class VisitorsController < ApplicationController
   def index
 
     @page = params[:page].nil? ? 1 : params[:page]
+    @sort_by = params[:sort_by].nil? ? "popular" : params[:sort_by]
 
     @api_key = "5c6aa64f2306d670d1918784955b7812"
-    request = HTTP.get("https://api.themoviedb.org/3/movie/popular?page=#{@page}&api_key=#{@api_key}&language=en-US")
+    request = HTTP.get("https://api.themoviedb.org/3/movie/#{@sort_by}?page=#{@page}&api_key=#{@api_key}&language=en-US")
     @data = JSON.parse(request)
 
     @movies = @data['results']
