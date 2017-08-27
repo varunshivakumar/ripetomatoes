fb5f3abfd8904bf214216b1d4f7a47ed9ee8d4ce
Varun Shivakumar <varunshivakumar@outlook.com>
2017-08-26 22:46:48 -0400

    Rewviews on movie page styling and base funtionality completed. Working on ajax

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def show
     @review = Review.new
     @movie = Tmdb::Movie.detail(params[:id])
-    @movie_reviews = Review.where(movie: @movie.id)
+    @movie_reviews = Review.where(movie: @movie.id).order('updated_at DESC')
   end
 
 
