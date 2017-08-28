4847e87e56577f7969c4345d2bdf7d7e792cad6b
Varun Shivakumar <varunshivakumar@outlook.com>
2017-08-28 06:24:43 -0400

    fixed migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825061507) do
-
-  create_table "movies", force: :cascade do |t|
-    t.integer  "db_id"
-    t.string   "title"
-    t.text     "summary"
-    t.float    "vote"
-    t.string   "poster"
-    t.string   "homepage"
-    t.string   "release_date"
-    t.integer  "runtime"
-    t.string   "status"
-    t.string   "tagline"
-    t.string   "backdrop"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
+ActiveRecord::Schema.define(version: 20170823230504) do
 
   create_table "reviews", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "rating"
-    t.string   "comment"
-    t.integer  "movie"
+    t.integer "user_id"
+    t.integer "rating"
+    t.string "comment"
+    t.integer "movie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-    t.integer  "role"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
